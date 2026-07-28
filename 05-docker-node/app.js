5b0dbebcb6905346c3fae9707d13a62eea504d03
Pankaj Prajapati <prajapatipankaj9569@gmail.com>
2026-07-28 20:30:27 +0530

    change hostname

--- 05-docker-node/app.js
+++ 05-docker-node/app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Docker Node app!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
